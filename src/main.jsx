3fe511f19aec5a2c4c1da924a19b5039450c60b8
wuzoo <wndyd9911@gmail.com>
2024-05-19 05:17:56 +0900

    feat: Header 컴포넌트 App에 추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { AppRouter } from './router/Router.jsx';
 import { GlobalStyle } from './styles/GlobalStyle.js';
-import { Theme } from './styles/theme.js';
+import { theme } from './styles/theme.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={Theme}>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <AppRouter />
     </ThemeProvider>
