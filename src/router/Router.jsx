9061cdb967dfdd8ad56dd657e74e73191cc0e799
yarimu <yhli0320@naver.com>
2024-05-19 01:34:51 +0900

    init: 초기세팅

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,16 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from '../App';
-import Funnel from '../pages/funnel/Funnel';
+
 import Home from '../pages/Home';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: '/test', element: <Funnel /> },
-    ],
+    children: [{ index: true, element: <Home /> }],
   },
 ]);
 
