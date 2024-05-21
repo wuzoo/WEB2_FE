f6c56136840fa5a2856a5ebd70271470cca84d5c
wuzoo <wndyd9911@gmail.com>
2024-05-21 13:26:29 +0900

    refactor: img 프로퍼티 디렉토리가 아닌 이미지로 값 수정

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,27 +1,29 @@
-/** 상수 데이터 작성 */
+import Sopkathon from '../assets/SceneMoonrae.png';
+import Lock from '../assets/lock.png';
+
 export const SceneData = [
   {
     id: 1,
     title: '설렘의 솝커톤',
     description: '버디버디조부터 솝커톤까지 같은 조에 배정된 우리. 이거 운명인가?',
-    img: 'src/assets/SceneMoonrae.png',
+    img: Sopkathon,
   },
   {
     id: 2,
     title: '너 내 마니또지!',
     description: '언제부턴가 내 자리에는 아메리카노와 초콜렛이... 이거 누구야?',
-    img: 'src/assets/lock.png',
+    img: Lock,
   },
   {
     id: 3,
     title: '스위트홈',
     description: '앱잼 3주간 합숙하게 된 우리. 하지만 첫날부터 예상치 못한 일들이 펼쳐지는데?!',
-    img: 'src/assets/lock.png',
+    img: Lock,
   },
   {
     id: 4,
     title: '첫 합동세미나',
     description: ' 합동세미나에서 마주친 학창시절 첫사랑! 그때나 지금이나 여전하네',
-    img: 'src/assets/lock.png',
+    img: Lock,
   },
 ];
